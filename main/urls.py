ddc197cd7c4e8d3d7bdf95a730b08b6ec777e0a6
MatsumotoKouta <kouta_matsumoto@demia.co.jp>
2025-09-24 14:59:51 +0900

    教材の修正に伴う修正

--- main/urls.py
+++ main/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<topic>/", views.forum, name="forum"),
-    path(
-        "<topic>/message/<int:pk>/delete/", views.delete_message, name="delete_message"
-    ),
+    path("<topic_name>/", views.forum, name="forum"),
+    path("<topic_name>/message/<int:pk>/delete/", views.delete_message, name="delete_message"),
 ]
--- main/views.py
+++ main/views.py
@@ -11,22 +11,24 @@ def index(request):
     return render(request, "main/index.html", context)
 
 
-def forum(request, topic):
-    topic = Topic.objects.get(name=topic)
+def forum(request, topic_name):
+    topic = Topic.objects.get(name=topic_name)
+    print(topic)
     messages = Message.objects.filter(topic=topic).order_by("created_at")
-    try:
+    if request.method == "POST":
         message = request.POST["message"]
         Message.objects.create(
             topic=topic,
             content=message,
         )
-    except:
-        pass
-    return render(request, "main/forum.html", {"messages": messages, "topic": topic})
+    context = {
+        "messages": messages,
+        "topic": topic
+    }
+    return render(request, "main/forum.html", context)
 
 
-def delete_message(request, topic, pk):
+def delete_message(request, topic_name, pk):
     message = get_object_or_404(Message, pk=pk)
-    if request.method == "POST":
-        message.delete()
-    return redirect("forum", topic=topic)
+    message.delete()
+    return redirect("forum", topic_name=topic_name)
