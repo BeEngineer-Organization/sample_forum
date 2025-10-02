d96cfbc6c842cf320bb3e721e608164b5b9439d0
MatsumotoKouta <kouta_matsumoto@demia.co.jp>
2025-10-02 19:10:21 +0900

    インデントミスを修正

--- main/templatetags/forum.py
+++ main/templatetags/forum.py
@@ -26,9 +26,9 @@ def elapsed_time(dt):
 
     if delta < one_hour:  # 経過時間が 1 時間以内のとき
             return f"{delta.seconds // 60} 分前"
-        elif delta < one_day:  # 経過時間が 1 日以内のとき
-            return f"{delta.seconds // 3600} 時間前"
-        elif delta < one_week:  # 経過時間が 1 週間以内のとき
-            return f"{delta.days} 日前"
-        else:
-            return "1 週間以上前"
+    elif delta < one_day:  # 経過時間が 1 日以内のとき
+        return f"{delta.seconds // 3600} 時間前"
+    elif delta < one_week:  # 経過時間が 1 週間以内のとき
+        return f"{delta.days} 日前"
+    else:
+        return "1 週間以上前"
