5e5f49a48adc6b9534362604c49c6b15ceaef1bc
MatsumotoKouta <kouta_matsumoto@demia.co.jp>
2025-10-02 13:46:18 +0900

    カスタムテンプレートタグを日本語に修正

--- main/templatetags/forum.py
+++ main/templatetags/forum.py
@@ -25,12 +25,10 @@ def elapsed_time(dt):
         raise ValueError("未来の時刻です。")
 
     if delta < one_hour:  # 経過時間が 1 時間以内のとき
-        minutes = delta.seconds // 60
-        return ngettext("%d minute ago", "%d minutes ago", minutes) % minutes
-    elif delta < one_day:  # 経過時間が 1 日以内のとき
-        hours = delta.seconds // 3600
-        return ngettext("%d hour ago", "%d hours ago", hours) % hours
-    elif delta < one_week:  # 経過時間が 1 週間以内のとき
-        return ngettext("%d day ago", "%d days ago", delta.days) % delta.days
-    else:
-        return gettext("more than 1 week")
+            return f"{delta.seconds // 60} 分前"
+        elif delta < one_day:  # 経過時間が 1 日以内のとき
+            return f"{delta.seconds // 3600} 時間前"
+        elif delta < one_week:  # 経過時間が 1 週間以内のとき
+            return f"{delta.days} 日前"
+        else:
+            return "1 週間以上前"
